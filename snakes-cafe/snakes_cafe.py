8a20af2125762c3d134923a73aa58703545200ca
osamadado123 <osama.aldado@gmail.com>
2022-10-03 15:56:37 +0000

    lab01

---
+++ snakes-cafe/snakes_cafe.py
@@ -0,0 +1,49 @@
+Appetizers = ["Wings","Cookies","Spring Rolls"]
+Entrees =["Salmon","Steak","Meat Tornado","A Literal Garden"]
+Desserts = ["Ice Cream","Cake","Pie"]
+Drinks =["Coffee","Tea","Unicorn Tears"]
+menu =["Wings","Cookies","Spring Rolls","Salmon","Steak","Meat Tornado","A Literal Garden","Ice Cream","Cake","Pie","Coffee","Tea","Unicorn Tears" ]
+print("**************************************")
+
+print("**    Welcome to the Snakes Cafe!   **")
+print("**    Please see our menu below.    **")
+print("**                                  **")
+print("** To quit ay any time, type \"quit\" **")
+
+print("**************************************")
+print("Appetizers\n---------")
+for x in Appetizers:
+  print(x)
+print("\n")
+print("Entrees\n-------")
+for x in Entrees:
+  print(x)
+print("\n")
+print("Desserts\n-------")
+for x in Desserts:
+  print(x)
+print("\n")
+print("------")
+for x in Drinks:
+  print(x)
+print("\n")
+print("***********************************")
+print("** What would you like to order? **")
+print("***********************************")
+keyword= "quit"
+num=0
+while True :
+ 
+ order = input("> ")
+ 
+ if order == keyword :
+    break
+ if order in menu : 
+    num+=1
+    print(f"** {num} order of {order} have been added to your meal **")
+ else : 
+    print("**           item is not on the menu           **")
+ 
+
+    
+     
